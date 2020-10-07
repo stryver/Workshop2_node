a4c0c415808c7cbd14c41b3a30c75432454717c8
GaulthierBOggio <gaulthier.boggio@epitech.eu>
2020-10-07 13:28:56 +0200

    create api user

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const country = require('./schema.js')
+const user = require('./schema.js')
 const app = express();
 
-mongoose.connect('',
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.lhnu2.mongodb.net/user',
     { useNewUrlParser: true,
         useUnifiedTopology: true })
     .then(() => console.log('Connected to database'))
     .catch(() => console.log('Error connection to database'));
 
 app.use(express.json());
-app.use(express.urlencoded());
 
-app.get('/country', (req, res, next) => {
-    country.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
+app.post('/login', async (req, res, next) => {
+    const reqUser = await user.findOne({ mail: req.body.mail})
+        if (reqUser.password === req.body.password) {
+            res.status(200).json({"message": "OK"});
+        } else {
+            res.status(403).json({"message": "Wrong id or password"})
+        }
+        res.status(404).json();
 });
 
-app.get('/country/:country', (req, res, next) => {
-    country.findOne({ country: req.params.country })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-
-app.post('/country', (req, res, next) => {
-    console.log(req.body);
-    const object = new country({
+app.post('/register', (req, res, next) => {
+    const object = new user({
         ...req.body
     });
     object.save()
-        .then(() => res.status(200).json({ message: 'Country saved'}))
+        .then(() => res.status(201).json({ message: 'User saved'}))
         .catch(error => res.status(400).json({ error }));
 });
 
--- schema.js
+++ schema.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const countrySchema = mongoose.Schema({
-    country: { type: String, required: true },
-    code: { type: String, required: true },
-    capital_city: { type: String, required: true },
-    continent: { type: String, required: true },
-    area: { type: Number, required: true },
+const userSchema = mongoose.Schema({
+    mail: { type: String, required: true },
+    password: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Country', countrySchema);
+module.exports = mongoose.model('User', userSchema);
